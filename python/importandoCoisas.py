f32e093acc0502572b1c979faa206918dd59865d
cugim <rafaelfelipe.martins18@gmail.com>
2021-01-26 22:46:26 -0300

    Create importandoCoisas.py

---
+++ python/importandoCoisas.py
@@ -0,0 +1,10 @@
+import random
+from math import sqrt, floor
+#num = int(input('Informe um número: '))
+#raiz = sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num,floor(raiz)))
+
+print('Numero aleatorio: ')
+#n = random.randint(1,10)
+#print(n)
+print('Emoji')
