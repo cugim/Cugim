70fef6bc640eee7291b22734d6974dd881c47629
cugim <rafaelfelipe.martins18@gmail.com>
2021-02-22 16:57:57 -0300

    Update ex29.py

--- python/ex29.py
+++ python/ex29.py
@@ -1,5 +1,6 @@
-vel = int(input('Informe a velocidade do carro: '))
+vel = float(input('Informe a velocidade do carro: '))
 if vel > 80:
     print('Voce foi multado! OTARIO, o limite é 80km/h voce está a {}km/h'.format(vel))
-else:
-    print('Voce está dentro do limite, sua velocidade é de {}'.format(vel))    
+    multa = (vel-80) * 7
+    print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
+print('Tenha um bom dia! Dirija com segurança!')
