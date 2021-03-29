7c093f5bcdeb775d3bc4f4142d99f69483d72267
cugim <rafaelfelipe.martins18@gmail.com>
2021-03-29 14:22:31 -0300

    refiz

--- ex059.py
+++ ex059.py
@@ -1,35 +1,31 @@
-from os import closerange
-from time import sleep
-n1 = int(input('Priemiro valor: '))
+n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
 opção = 0
-while opção != 5:
+while opção != 5:        
     print('''    [ 1 ] Somar
-    [ 2 ] Multiplicar
+    [ 2 ] multiplicar
     [ 3 ] Maior
-    [ 4 ] Novos números
+    [ 4 ] Novos Números
     [ 5 ] Sair do programa''')
     opção = int(input('Qual é a sua opção? '))
-    if opção  == 1:
+    if opção == 1:
         soma = n1 + n2
-        print('A soma entre {} e {} é {}'.format(n1, n2, soma))
+        print('A soma de {} + {} é {}'.format(n1, n2, soma))
     elif opção == 2:
-        produto = n1 * n2
-        print('A multiplicação de {} e {} da {}'.format(n1, n2, produto))
+        mult = n1 * n2
+        print('O resultado de {} X {} é {}'.format(n1, n2, mult))
     elif opção == 3:
         if n1 > n2:
             maior = n1
-        else:  
-            maior   = n2
-        print('O maior entre {} e {} é {}'.format(n1, n2, maior))
+        else:
+            maior = n2
+        print('Entre {} e {} o maior é {}'.format(n1, n2, maior))
     elif opção == 4:
-        print('Informe os números novamente: ')
+        print('Informe os numeros novamente')
         n1 = int(input('Primeiro valor: '))
         n2 = int(input('Segundo valor: '))
     elif opção == 5:
-        print('Finalizando...')
-    else: 
-        print('Opção inválida. Tente novamente')
-    print('=-=' * 10)
-    sleep(1)
-print('Fim do programa! Volte sempre')
+        print('Volte sempre')
+    else:
+        print('Opção inválida tente novamente!')    
+print('Fim do programa...')
