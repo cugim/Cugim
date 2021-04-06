1e6044eeb7d06f09cc6b72e7e1a46dd4bf3caf0f
cugim <rafaelfelipe.martins18@gmail.com>
2021-04-06 18:15:21 -0300

    nem sei o que to fazendo

--- python/ex065.py
+++ python/ex065.py
@@ -1,8 +1,8 @@
 resp = 'S'
-soma = quant = media = maior = menor = 0
+soma = quant = media = maior = 0
 while resp in 'Ss':
-    num = int(input('Digite um numero: '))
-    soma += num
+    num = int(input('Digite um número: '))
+    soma += num 
     quant += 1
     if quant == 1:
         maior = menor = num
@@ -11,8 +11,7 @@ while resp in 'Ss':
             maior = num
         if num < menor:
             menor = num
-    resp = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
-media = soma/quant
-print('Voce digitou {} numeros e a media foi {}'.format(quant, media))
-print('O maior valor oi {} e o menor foi {}'.format(maior, menor))
-
+        resp = str(input('Quer continuar? [S/N]: ')).upper().strip()
+media = soma / quant
+print('Voce digitou {} numero e a media foi {}'.format(quant, media))
+print('O maior valor foi {} e  menor foi {}'.format(maior, menor))
